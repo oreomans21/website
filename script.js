693950445cf279c90f4ed36656f083de8d54f644
oreomans21 <150438259+oreomans21@users.noreply.github.com>
2023-11-10 12:53:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new fullpage('#fullpage', {
+        sectionsColor: ['#3498db', '#2ecc71', '#e74c3c'],
+        navigation: true,
+        scrollingSpeed: 1000
+    });
+});
